e4e328174c958e2a67c2184a05c29c0671651e0a
Tanawat <scomusicrusted@gmail.com>
2017-12-22 06:15:54 +0000

    ake changes in translation

--- resources/views/cart/mycart.blade.php
+++ resources/views/cart/mycart.blade.php
@@ -17,7 +17,7 @@
 <div class="container">
   <div class="shop-panel">
     <div class="panel-heading">
-      <h3 class="no-margin">My cart</h3>
+      <h3 class="no-margin">{{__('message.cart')}}</h3>
     </div>
     <div class="cart-wrap panel-body">
       <cart user-id="{{ Auth::check() ? Auth::user()->id : 'null' }}" user-name="{{ Auth::check() ? Auth::user()->name : 'null' }}"></cart>
