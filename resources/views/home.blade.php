a230c497d16047232f1b5c5c35dad8cb0b5266dd
Tanawat <scomusicrusted@gmail.com>
2017-12-22 06:55:43 +0000

    update banner links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,8 @@
             <div class="large-panel">
                 <div class="top-grid">
                   <div class="top-banner">
-                    <img src="https://s3.console.aws.amazon.com/s3/buckets/images.closet.com/banner/" alt="banner">
-                    <img src="https://s3.console.aws.amazon.com/s3/buckets/images.closet.com/banner/" alt="banner">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/images.closet.com/banner/banner1.jpg" alt="banner">
+                    <img src="https://s3-ap-southeast-1.amazonaws.com/images.closet.com/banner/banner2.jpg" alt="banner">
                   </div>
                   <div class="banner-side-nav">
                     <ul >
