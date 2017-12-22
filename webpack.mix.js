9c3cebaef7741ca96bb1e72feb977d9da4b20d83
Tanawat <scomusicrusted@gmail.com>
2017-12-22 06:29:24 +0000

    ake changes in css

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,10 @@ const { mix } = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js([
-	'resources/assets/js/all.js',
-	'resources/assets/js/app.js',
-	], 'public/js/main.js');
+// mix.js([
+// 	'resources/assets/js/all.js',
+// 	'resources/assets/js/app.js',
+// 	], 'public/js/main.js');
 
 mix.styles([
 	'resources/assets/css/general.css',
