28e8f0ace352fb004a1e44d24bbd3fb0991bedb6
vagrant <vagrant@homestead>
2018-09-06 09:35:52 +0000

    fix admin seed

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Closet\Models\User;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
@@ -11,39 +12,68 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('users')->insert([
-        'id' => 0,
-        'name' => 'Closet Official',
-        'email' => 'admin@closet.plus',
-        'password' => bcrypt('ThisIsCloset2018'),
-        'verified' => true,
-        'email_token' => 'Master_Key',
-        'address' => 'not specified',
-        'phone' => 'not specified',
-        'country' => 'th',
-        'gender' => 'men',
+      $user = User::create([
+          'name' => 'Closet Official',
+          'email' => 'admin@closet.plus',
+          'password' => bcrypt('ThisIsCloset2018'),
+          'verified' => true,
+          'email_token' => 'Master_Key',
+          'address' => 'not specified',
+          'phone' => 'not specified',
+          'country' => 'th',
+          'gender' => 'men',
       ]);
 
-      DB::table('shops')->insert([
-        'id' => 0,
-        'user_id' => 0,
-        'usertype' => 99,
-        'profile_type' => 1,
-        'name' => 'Closet',
-        'slug' => 'closet',
-        'description' => 'not specified',
-        'thumbnail' => 'closet_thumbnail.jpg',
-        'cover' => 'closet_cover.jpg',
-        'view_count' => 0,
-        'shipping' => null,
+      $shop = $user->shop()->create([
+          'usertype' => 99,
+          'profile_type' => 1,
+          'name' => 'Closet',
+          'slug' => 'closet',
+          'description' => 'not specified',
+          'thumbnail' => 'closet_thumbnail.jpg',
+          'cover' => 'closet_cover.jpg',
+          'view_count' => 0,
+          'shipping' => null,
       ]);
 
-      DB::table('shop_promotions')->insert([
-        'id' => 0,
-        'shop_id' => 0,
-        'discount' => 99,
-        'get_another' => 99,
-        'flash_sale' => 99,
+      $shop->availablePromotions()->create([
+        'discount' => 5,
+        'get_another' => 5,
+        'flash_sale' => 2,
       ]);
+      // DB::table('users')->insert([
+      //   'id' => 0,
+      //   'name' => 'Closet Official',
+      //   'email' => 'admin@closet.plus',
+      //   'password' => bcrypt('ThisIsCloset2018'),
+      //   'verified' => true,
+      //   'email_token' => 'Master_Key',
+      //   'address' => 'not specified',
+      //   'phone' => 'not specified',
+      //   'country' => 'th',
+      //   'gender' => 'men',
+      // ]);
+      //
+      // DB::table('shops')->insert([
+      //   'id' => 0,
+      //   'user_id' => 0,
+      //   'usertype' => 99,
+      //   'profile_type' => 1,
+      //   'name' => 'Closet',
+      //   'slug' => 'closet',
+      //   'description' => 'not specified',
+      //   'thumbnail' => 'closet_thumbnail.jpg',
+      //   'cover' => 'closet_cover.jpg',
+      //   'view_count' => 0,
+      //   'shipping' => null,
+      // ]);
+      //
+      // DB::table('shop_promotions')->insert([
+      //   'id' => 0,
+      //   'shop_id' => 0,
+      //   'discount' => 99,
+      //   'get_another' => 99,
+      //   'flash_sale' => 99,
+      // ]);
     }
 }
