a1074720790b301cb96ab847080b17c1af161fd5
vagrant <vagrant@homestead>
2018-10-04 05:31:28 +0000

    fix route closure

--- app/Http/Controllers/Api/middleware.php
+++ app/Http/Controllers/Api/middleware.php
@@ -7,27 +7,8 @@ use Closet\Http\Controllers\Controller;
 
 class MiddlewareApi extends Controller
 {
-  public function getShop($id)
+  public function api(Request $request)
   {
-    $data = Shop::find($id);
-
-    return response()->json([
-      'id' => $data->id,
-      'name' => $data->name,
-      'slug' => $data->slug,
-    ]);
-  }
-  public function getBankAccount($id)
-  {
-    $data = DB::table('accounts')->where('shop_id', $id)->get();
-
-    return response()->json($data);
-  }
-
-  public function getMyProduct(Request $request)
-  {
-    $data = $request->user()->products()->latestFirst()->get();
-
-    return response()->json($data);
+    return $request->user();
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', 'Api\MiddlewareApi');
+Route::middleware('auth:api')->get('/user', 'Api\MiddlewareApi@api');
