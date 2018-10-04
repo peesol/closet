5511993d1d316423b01d94d7583e5b7c6ff0a5fe
vagrant <vagrant@homestead>
2018-10-04 05:26:33 +0000

    fix route closure

---
+++ app/Http/Controllers/Api/middleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Closet\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Closet\Http\Controllers\Controller;
+
+class MiddlewareApi extends Controller
+{
+  public function getShop($id)
+  {
+    $data = Shop::find($id);
+
+    return response()->json([
+      'id' => $data->id,
+      'name' => $data->name,
+      'slug' => $data->slug,
+    ]);
+  }
+  public function getBankAccount($id)
+  {
+    $data = DB::table('accounts')->where('shop_id', $id)->get();
+
+    return response()->json($data);
+  }
+
+  public function getMyProduct(Request $request)
+  {
+    $data = $request->user()->products()->latestFirst()->get();
+
+    return response()->json($data);
+  }
+}
