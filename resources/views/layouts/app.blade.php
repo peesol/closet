5a50442bb94ee9d4e78871647c2ce9c8812eb241
vagrant <vagrant@homestead>
2018-11-02 05:02:51 +0000

    add FB product catalog tracking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @hasSection('og-image')
-  <meta property="og:title" content="@yield('og-title')">
+    @yield('og-title')
   @else
   <meta property="og:title" content="@yield('title'){{config('app.name') }}">
   @endif
