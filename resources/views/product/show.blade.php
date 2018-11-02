f16c17ff65669fce1501db6cdf9fc170e1e893d5
vagrant <vagrant@homestead>
2018-11-02 05:31:46 +0000

    add FB product catalog tracking

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -2,25 +2,12 @@
 @section('title')
 {{$product->name.' - '}}
 @endsection
-
 @section('og-image')
   @if (count($product->productimages))
   https://s3-ap-southeast-1.amazonaws.com/files.closet/product/photo/{{ $product->productimages->first()->filename}}
   @else
   https://s3-ap-southeast-1.amazonaws.com/files.closet/product/thumbnail/{{ $product->thumbnail }}
   @endif
-@endsection
-@section('og-title')
-<meta property="og:title" content="{{ $product->name }}">
-@endsection
-@section('og-catalog')
-<meta property="og:url" content="{{ config('app.url') . '/product/' . $product->uid }}">
-<meta property="product:brand" content="unidentified">
-<meta property="product:availability" content="{{ $product->stock ? 'in stock' : 'out of stock' }}">
-<meta property="product:condition" content="new">
-<meta property="product:price:amount" content="{{ $product->price }}">
-<meta property="product:price:currency" content="THB">
-<meta property="product:retailer_item_id" content="{{ $product->id }}">
 @endsection
 @section('fb-events')
 <script>
@@ -37,7 +24,18 @@ window.addEventListener("load",function(){var t=document.querySelectorAll(".tab-
 </script>
 @endsection
 @section('content')
-
+<header>
+ <meta property="og:title" content="{{ $product->name }}">
+ <meta property="og:description" content="{{ $product->description }}">
+ <meta property="og:url" content="{{ config('app.url') . '/product/' . $product->uid }}">
+ <meta property="og:image" content="https://s3-ap-southeast-1.amazonaws.com/files.closet/product/thumbnail/{{ $product->thumbnail }}">
+ <meta property="product:brand" content="unidentified">
+ <meta property="product:availability" content="{{ $product->stock ? 'in stock' : 'out of stock' }}">
+ <meta property="product:condition" content="new">
+ <meta property="product:price:amount" content="{{ $product->price }}">
+ <meta property="product:price:currency" content="THB">
+ <meta property="product:retailer_item_id" content="{{ $product->id }}">
+</header>
 <div class="container">
   @include('product.partials._login_warn')
             <div class="medium-panel">
